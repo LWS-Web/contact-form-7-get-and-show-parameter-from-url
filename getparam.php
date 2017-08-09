09a479f7a3485319da5b52aeee789acf7f0b15d6
LWS <moritz.heier@lws-werbung.de>
2017-08-09 11:01:41 +0200

    Make it work again on CF7-4.8.1 and WP-4.8.1
    CHANGED:
    WPCF7_Shortcode Has Become Deprecated
    WPCF7_Shortcode class is now marked as deprecated. If you use WPCF7_Shortcode in your custom code, you will need to replace it with 
    WPCF7_FormTag. Otherwise, you will receive a warning if you are in the debug mode.
    Link: https://contactform7.com/2016/12/03/contact-form-7-46/
    
    CHANGED:
    $tag is no longer an array, but is now an object.
    Link: https://wordpress.org/support/topic/not-yet-working-with-contact-form-7-v4-8/
    Thanks to PascalFuchs (https://wordpress.org/support/users/pascalfuchs/)

--- getparam.php
+++ getparam.php
@@ -1,15 +1,17 @@
 <?php
 /**
- * Plugin Name: Contact Form 7 Get and Show Parameter from URL
- * Plugin URI: http://elementdesignllc.com/2011/11/contact-form-7-get-parameter-from-url-into-form-plugin/
- * Description: Get and Show Parameter from URL Contact Form 7 Plugin
- * Version: 0.9.7
- * Author: Chad Huntley
- * Author URI: http://URI_Of_The_Plugin_Author
+ * Plugin Name: Contact Form 7 Get and Show Parameter from URL (Fixed)
+ * Plugin URI: https://github.com/LWS-Web/contact-form-7-get-and-show-parameter-from-url/
+ * Description: Get and Show Parameter from URL with Contact Form 7 Plugin.
+ * Version: 0.9.7a
+ * Author: Chad Huntley, Mo
+ * Author URI: https://github.com/LWS-Web/
  * License: GPL2
  */
 
-/*  Copyright 2013  Chad Huntley  (email : chad@elementdesignllc.com)
+/*  Original code, copyright 2013  Chad Huntley  (email : chad@elementdesignllc.com)
+
+    Code modified to work with new WP and CF7 versions, 2017 Moritz Heier (email : moritz.heier@lws-werbung.de)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as
@@ -28,14 +30,25 @@
 add_action( 'wpcf7_init', 'wpcf7_add_shortcode_getparam' );
 
 function wpcf7_add_shortcode_getparam() {
-    if ( function_exists( 'wpcf7_add_shortcode' ) ) {
-        wpcf7_add_shortcode( 'getparam', 'wpcf7_getparam_shortcode_handler', true );
-        wpcf7_add_shortcode( 'showparam', 'wpcf7_showparam_shortcode_handler', true );
+    /* CHANGED
+     * WPCF7_Shortcode Has Become Deprecated
+     * WPCF7_Shortcode class is now marked as deprecated. If you use WPCF7_Shortcode in your custom code, you will need to replace it with 
+     * WPCF7_FormTag. Otherwise, you will receive a warning if you are in the debug mode.
+     * Link: https://contactform7.com/2016/12/03/contact-form-7-46/
+     */
+    if ( function_exists( 'wpcf7_add_form_tag' ) ) {
+        wpcf7_add_form_tag( 'getparam', 'wpcf7_getparam_shortcode_handler', true );
+        wpcf7_add_form_tag( 'showparam', 'wpcf7_showparam_shortcode_handler', true );
     }
 }
 
 function wpcf7_getparam_shortcode_handler($tag) {
-    if (!is_array($tag)) return '';
+    /* CHANGED
+     * $tag is no longer an array, but is now an object.
+     * Link: https://wordpress.org/support/topic/not-yet-working-with-contact-form-7-v4-8/
+     * Thanks to PascalFuchs (https://wordpress.org/support/users/pascalfuchs/)
+     */
+    if (!is_object($tag)) return '';
 
     $name = $tag['name'];
     if (empty($name)) return '';
@@ -45,7 +58,12 @@ function wpcf7_getparam_shortcode_handler($tag) {
 }
 
 function wpcf7_showparam_shortcode_handler($tag) {
-    if (!is_array($tag)) return '';
+    /* CHANGED
+     * $tag is no longer an array, but is now an object.
+     * Link: https://wordpress.org/support/topic/not-yet-working-with-contact-form-7-v4-8/
+     * Thanks to PascalFuchs (https://wordpress.org/support/users/pascalfuchs/)
+     */
+    if (!is_object($tag)) return '';
 
     $name = $tag['name'];
     if (empty($name)) return '';
